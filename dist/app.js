47208edf3695bf44982e01e91c78545d2f283608
jtdev632 <jaccek.bid@gmail.com>
2024-08-07 22:11:38 +0200

    add local storage- beta

--- dist/app.js
+++ dist/app.js
@@ -18,16 +18,20 @@ document.addEventListener("DOMContentLoaded", () => {
             return;
         }
         item.addItem();
+        const myObj = { "name": item.name, "weight": item.weight, "minValue": item.minValue, "midValue": item.midValue };
+        const myJSON = JSON.stringify(myObj);
+        localStorage.setItem("testJSON", myJSON);
         document.querySelectorAll('li').forEach(element => {
             element.addEventListener('click', () => {
                 item.weight = prompt("Enter a new value for weight");
-                if (item.weight < 0 || item.weight == "") {
+                if (item.weight < 0 || item.weight == "" || item.weight == null || isNaN(item.weight)) {
                     errorMessage.textContent = 'Invalid input. Please enter a positive number for weight.';
                     return;
                 }
+                let text = localStorage.getItem("testJSON");
+                let obj = JSON.parse(text);
                 item.weight = parseFloat(item.weight);
-                element.textContent = `${item.name} - Weight: ${item.weight} kg`;
-                
+                element.textContent = `${item.name} - Weight: ${item.weight} kg ${obj.minValue}`;
             });
         });
         errorMessage.textContent = "";
@@ -42,8 +46,11 @@ document.addEventListener("DOMContentLoaded", () => {
         this.weight = weight;
         this.minValue = minValue;
         this.midValue = midValue;
-        this.addItem = () => {
-            itemList.innerHTML += `<li class='mb-8 max-w-md mx-auto p-4 bg-green-400 rounded shadow-md border border-gray-300 transform transition-transform duration-200 hover:scale-105'>${this.name} - Weight: ${this.weight} kg</li>`;
+        this.addItem = function (itemList) {
+            const listItem = document.createElement('li');
+            listItem.classList.add('mb-8', 'max-w-md', 'mx-auto', 'p-4', 'bg-green-400', 'rounded', 'shadow-md', 'border', 'border-gray-300', 'transform', 'transition-transform', 'duration-200', 'hover:scale-105');
+            listItem.textContent = `${this.name} - Weight: ${this.weight} kg`;
+            itemList.appendChild(listItem);
         };
     };
 });
--- src/app.ts
+++ src/app.ts
@@ -22,17 +22,22 @@ itemForm.addEventListener('submit', (e)=> {
         return;
     } 
     item.addItem();
-    
+    const myObj = {"name":item.name, "weight":item.weight, "minValue":item.minValue, "midValue":item.midValue}
+    const myJSON = JSON.stringify(myObj);
+    localStorage.setItem("testJSON", myJSON);
         
     document.querySelectorAll('li').forEach(element =>{
     element.addEventListener('click', () => {
          item.weight = prompt("Enter a new value for weight")
-         if(item.weight < 0 || item.weight == ""){
+         if(item.weight < 0 || item.weight == ""  || item.weight == null || isNaN(item.weight)){
              errorMessage.textContent = 'Invalid input. Please enter a positive number for weight.'
              return;
             }
-        item.weight = parseFloat(item.weight);
-         element.textContent = `${item.name} - Weight: ${item.weight} kg`;
+            let text = localStorage.getItem("testJSON");
+            let obj = JSON.parse(text);
+            item.weight = parseFloat(item.weight);
+         element.textContent = `${item.name} - Weight: ${item.weight} kg ${obj.minValue}`;
+
     })
  
 })
@@ -53,8 +58,11 @@ const Parameters = function (name:string, weight:number, minValue:number, midVal
     this.weight = weight;
     this.minValue = minValue;
     this.midValue = midValue;
-    this.addItem = () => {
-        itemList.innerHTML += `<li class='mb-8 max-w-md mx-auto p-4 bg-green-400 rounded shadow-md border border-gray-300 transform transition-transform duration-200 hover:scale-105'>${this.name} - Weight: ${this.weight} kg</li>`;
+    this.addItem = function(itemList) {
+        const listItem = document.createElement('li');
+        listItem.classList.add('mb-8', 'max-w-md', 'mx-auto', 'p-4', 'bg-green-400', 'rounded', 'shadow-md', 'border', 'border-gray-300', 'transform', 'transition-transform', 'duration-200', 'hover:scale-105');
+        listItem.textContent = `${this.name} - Weight: ${this.weight} kg`;
+        itemList.appendChild(listItem);
     }
     
 }
