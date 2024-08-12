006854f87416d82f675d886fad27854700787972
jtdev632 <jaccek.bid@gmail.com>
2024-08-12 16:23:50 +0200

    Revert "add trash icon"
    This reverts commit 67cd4d366c89da5771a47abd3771fa2a8766a2fe.

--- dist/app.js
+++ dist/app.js
@@ -58,9 +58,7 @@ document.addEventListener("DOMContentLoaded", () => {
             contentWrapper.appendChild(buttonPlus);
             const deleteButton = document.createElement('button');
             deleteButton.className = 'absolute top-2 right-2 bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded';
-            const icon = document.createElement('box-icon');
-            icon.setAttribute('name', 'trash');
-            deleteButton.appendChild(icon);
+            deleteButton.textContent = 'Delete';
             contentWrapper.appendChild(deleteButton);
             listItem.appendChild(contentWrapper);
             const itemId = id || Date.now().toString();
